c3e444da3a6625ce3422de84b9fbc3e6799155e5
Ersun Warncke <ersun.warncke@gmail.com>
2017-02-19 17:21:17 -0400

    baseic form class

--- test/immutable-core-model-form.js
+++ test/immutable-core-model-form.js
@@ -5,8 +5,11 @@ const ImmutableCoreModelForm = require('../lib/immutable-core-model-form')
 
 const chai = require('chai')
 const immutable = require('immutable-core')
+const chaiSubset = require('chai-subset');
 
 const assert = chai.assert
+
+chai.use(chaiSubset)
 
 describe('immutable-core-model-form', function () {
 
@@ -16,6 +19,7 @@ describe('immutable-core-model-form', function () {
         properties: {
             addressCountry: {
                 default: 'US',
+                description: 'ISO Country Code',
                 title: 'Country',
                 type: 'string',
             },
@@ -64,6 +68,62 @@ describe('immutable-core-model-form', function () {
         })
         // should return new instance
         assert.isTrue(addressForm instanceof ImmutableCoreModelForm)
+
+        assert.containSubset(addressForm, {
+            fields: [
+                {
+                    property: 'addressCountry',
+                    label: 'Country',
+                    inputType: 'text',
+                    required: true,
+                    description: 'ISO Country Code',
+                },
+                {
+                    property: 'addressLocality',
+                    label: 'City',
+                    inputType: 'text',
+                    required: true,
+                },
+                {
+                    property: 'addressRegion',
+                    label: 'State',
+                    inputType: 'select',
+                    options: [
+                        {key: 'AL', value: 'AL'},
+                        {key: 'AK', value: 'AK'},
+                        {key: 'AZ', value: 'AZ'},
+                    ],
+                    required: true,
+                },
+                {
+                    property: 'firstName',
+                    label: 'First Name',
+                    inputType: 'text',
+                    required: true,
+                },
+                {
+                    property: 'lastName',
+                    label: 'Last Name',
+                    inputType: 'text',
+                    required: true,
+                },
+                {
+                    property: 'postalCode',
+                    label: 'ZIP Code',
+                    inputType: 'text',
+                    required: true,
+                    pattern: '^d{5}$',
+                },
+                {
+                    property: 'streetAddress',
+                    label: 'Street Address',
+                    inputType: 'text',
+                    required: true,
+                },
+            ],
+            labels: true,
+            placeholders: false
+        })
     })
 
 })
